bf7ff15c17a71a2a281ecc931df4e57d222670f1
Kumar Sunderam <68020132+sandy1312@users.noreply.github.com>
2020-09-11 17:34:33 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,10 @@
 n=int(input("Enter ur number:"))
-def fib():
-    a=-1
-    b=1
-    if n<=0:
-        print("No output")
-    else:
-        for i in range(n):
-            c=a+b
-            a,b=b,c
-            print(c)
-fib()
+a=-1
+b=1
+if n<=0:
+    print("No output")
+else:
+    for i in range(n):
+        c=a+b
+        a,b=b,c
+        print(c,end=" ")
